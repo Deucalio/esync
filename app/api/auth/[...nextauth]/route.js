f625e9a998a33bff9774da026faf7aea35ee8063
Deucalio <hjamad89@gmail.com>
2024-01-22 20:03:34 +0500

    fix level

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,11 +5,12 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = nextAuth({
   providers: [
     GoogleProvider({
-      clientId: "373888449280-t3nijpkkc53p5lq2es7o49buhjng6hbi.apps.googleusercontent.com",
+      clientId:
+        "373888449280-t3nijpkkc53p5lq2es7o49buhjng6hbi.apps.googleusercontent.com",
       clientSecret: "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",
     }),
-    secret: "asdasdasdasd"
   ],
+  secret: "asdasdasdasd",
 });
 
 export { handler as GET, handler as POST };
